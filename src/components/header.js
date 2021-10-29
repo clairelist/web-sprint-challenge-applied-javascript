1115f3426d8149221117075cecb7a79dc6be0bc9
Claire List <claire.list2501@gmail.com>
2021-10-29 09:51:05 -0600

    task 2 compleat

--- src/components/header.js
+++ src/components/header.js
@@ -43,6 +43,10 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+  const headerStr = Header();
+  document.querySelector(selector).appendChild(headerStr);
+
 }
 
 export { Header, headerAppender }
